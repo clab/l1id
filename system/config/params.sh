ac99fe0e32301a37bc172623054c713e5315f295
ytsvetko <yulia.tsvetkov@gmail.com>
2013-03-11 22:30:02 -0400

    translationese features:mean_word_ranks

--- system/config/params.sh
+++ system/config/params.sh
@@ -2,7 +2,7 @@
 
 # Name of the experiment. This also defines which directory the output will be
 # stored in (under ../data/work/).
-export EXPERIMENT_NAME=ratio_to_passive_verbs
+export EXPERIMENT_NAME=mean_word_ranks
 
 # Which features should be extracted in this experiment.
 # Set to 1 features you want to be included in this experiment.
@@ -14,7 +14,8 @@ export ADD_CHARACTERS_FEATURES=0
 export ADD_PRONOUNS_FEATURES=0
 export ADD_REPETITIONS_FEATURES=0
 export ADD_POSITIONAL_TOKEN_FEATURES=0
-export ADD_RATIO_TO_PASSIVE_VERBS_FEATURES=1
+export ADD_RATIO_TO_PASSIVE_VERBS_FEATURES=0
+export ADD_MEAN_WORD_RANKS_FEATURES=1
 
 # Directory with training texts
 export TRAINING_INPUT_DIR=${INPUT_DIR}/NLI_2013_Training_Data/tokenized
@@ -69,5 +70,7 @@ if [ "${ADD_RATIO_TO_PASSIVE_VERBS_FEATURES}" == "1" ] ; then
   EXTRACT_FEATURES_PARAMS+="--append_ratio_to_passive_verbs=True "
   EXTRACT_FEATURES_PARAMS+="--pos_tagged_dir_passive_verbs=${POS_TAGGED_DIR} "
 fi
-
+if [ "${ADD_MEAN_WORD_RANKS_FEATURES}" == "1" ] ; then
+  EXTRACT_FEATURES_PARAMS+="--append_mean_word_ranks_features=True "
+fi
 export EXTRACT_FEATURES_PARAMS
---
+++ system/src/feature_extractors/mean_word_ranks.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+""" Translationese feature:
+We assume that less frequent words are used more often in original texts than
+in translated ones.
+"""
+
+import collections
+import sys
+import gflags
+import feature_extractor
+from word_ranks import WORD_RANKS
+
+FLAGS = gflags.FLAGS
+
+gflags.DEFINE_bool("append_mean_word_ranks_features", False,
+    "Append mean word rank.")
+gflags.DEFINE_integer("mean_word_ranks_variant", 0,
+    "Words not in the list are ignored altogether(1) or given very high rank(0).")
+
+VERY_HIGH_RANK = 6000
+"""Very high rank for a word, guessed for unknown words. The highest rank
+for known words is 5000."""
+
+class MeanWordRanksFeatureExtractor(feature_extractor.FeatureExtractor):
+  def ExtractFeaturesFromInstance(self, text, language, filename):
+    count = 0
+    rank_sum = 0
+    for word in text.split():
+      if not word.isalpha():
+        continue
+      if word in WORD_RANKS:
+        rank_sum += WORD_RANKS[word]
+        count += 1
+      else:
+        if FLAGS.mean_word_ranks_variant == 0:
+          rank_sum += VERY_HIGH_RANK
+          count += 1
+    return { "MeanWordRank": float(rank_sum) / count }
+  
+if __name__ == '__main__':
+  print "This module is a library, not supposed to be executed directly."
+  sys.exit(1)
+
+def REGISTER_FEATURE_EXTRACTOR():
+  if not FLAGS.append_mean_word_ranks_features:
+    return None
+  return MeanWordRanksFeatureExtractor()
+
