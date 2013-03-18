dc009f9ab5046b292d373bbcfd3f80d91c3bc0bb
Nathan Schneider <nschneid@cs.cmu.edu>
2013-03-18 16:41:53 -0400

    misspelling features: debugging information on error

--- system/src/feature_extractors/misspelling_features.py
+++ system/src/feature_extractors/misspelling_features.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+from __future__ import print_function
 import collections
 import math
 import sys
@@ -25,14 +25,18 @@ class MisspellingsFeatureExtractor(feature_extractor.FeatureExtractor):
     result = {}
     # /home/cdyer/l1id/system/data/input/NLI_2013_Training_Data/tokenized/1054261.txt
     corrected = re.sub(r'/tokenized/', r'/corrected/', filename)
-    for feat in subprocess.check_output([self.cdpath,filename,corrected]).split('\n'):
-      if len(feat) > 4:
-        (f, val) = feat.split('\t')
-        result[f] = float(val)
+    try:
+        for feat in subprocess.check_output([self.cdpath,filename,corrected]).split('\n'):
+          if len(feat) > 4:
+            (f, val) = feat.split('\t')
+            result[f] = float(val)
+    except OSError:
+        print([self.cdpath,filename,corrected], file=sys.stderr)
+        raise
     return result
 
 if __name__ == '__main__':
-  print "This module is a library, not supposed to be executed directly."
+  print("This module is a library, not supposed to be executed directly.", file=sys.stderr)
   sys.exit(1)
 
 def REGISTER_FEATURE_EXTRACTOR():
